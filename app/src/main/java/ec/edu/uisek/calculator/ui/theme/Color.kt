c4a47292fd631a3d53df6b564e0d4d3a45af6e50
laboratorio1.fs <arely.pazmino@uisek.edu.ec>
2025-10-15 10:56:58 -0500

    Lab2 hecho

--- app/src/main/java/ec/edu/uisek/calculator/ui/theme/Color.kt
+++ app/src/main/java/ec/edu/uisek/calculator/ui/theme/Color.kt
@@ -4,11 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink80 = Color(0xFFF6A2B7)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF5F8532)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val UiSekBlue = Color(0xFF03A9F4)
+val UiSekBlue = Color(0xFF62BAEF)
 val Red = Color(0xFF880030)
